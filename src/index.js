772ea2e5e21d5cae8b64c32c5d6ed921986b33c7
Jose Abelenda <jose.abelenda@liferay.com>
2022-01-03 11:56:19 -0300

    Adapt index.js to RemoteApp

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class FirstApplicationWebComponent extends HTMLElement {
+	connectedCallback() {
+		ReactDOM.render(
+			<App />,
+			this
+		);
+	}
+}
+
+const ELEMENT_ID = 'liferay-first-application';
+
+if (!customElements.get(ELEMENT_ID)) {
+	customElements.define(ELEMENT_ID, FirstApplicationWebComponent);
+}
